19c8d56aab3b9f28838de9ab86f3cb5bac0ed2f9
Krishna <hirdiyanshshukla@gmail.com>
2025-07-18 19:00:30 +0530

    allowed hosts to *

--- serveAPI/serveAPI/settings.py
+++ serveAPI/serveAPI/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-g2e%2y$=_@cb+32708cmi-&36c(^q=x2xc+%(139x2nmhbh5^8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
